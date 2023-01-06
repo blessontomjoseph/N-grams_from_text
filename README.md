d346e51896d871fb59936b2d729d71de2644b772
tom <57592961+blessontomjoseph@users.noreply.github.com>
2023-01-06 17:57:16 +0530

    documentation

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# N-grams_from_text
+# function for N-grams_from_text
+
+The code is an implementation of a custom function for extracting ngrams from a corpus given the path to the corpus and a value for n 
+
+- returns a dictionary with unique ngrams as the keys and its frequency in the corpus as the value
+- if ngrams of specified 'n' not present in the corpus, function return an empty dictionary
+
--- file.py
+++ file.py
@@ -1,4 +1,13 @@
 def ngrams(n, path):
+    """produces a dictionary with unique ngrams as keys and its frequency as values from a corpus.
+
+    Args:
+        n (int): ngram size
+        path (str): path to text file of corpus
+
+    Returns:
+        dict: unique ngram pairs as key, and its frequency as values.
+    """
     file = open(path, 'r')
     text = file.read()
     sentences = text.split('\n')
